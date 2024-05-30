8ef8cce4d533b0cd3fee1c335b12eb6e95983c6c
AlexFangSW <alexfangsw@gmail.com>
2024-05-30 10:54:07 +0800

    wip: some notes about the direction of the project

---
+++ backend/db/querys/blogs.sql
@@ -0,0 +1,8 @@
+--name: CreateBlog: one
+
+-- hmmm... should I just return id or everything
+--  - content might be very large
+--  - will I actually use this retruned data ??
+--  - I think I should create different functions, 
+--    one returns no content, and another with content.
+
