8e53679ed9f8610ab47cae8e08bb285af0cb8425
AlexFangSW <alexfangsw@gmail.com>
2024-05-31 11:14:18 +0800

    wip: template for queries

--- backend/db/querys/blogs.sql
+++ backend/db/querys/blogs.sql
@@ -1,5 +1,23 @@
 --name: CreateBlog: one
 
+--name: ListBlogs: many
+
+--name: ListBlogsDeleted: many
+
+--name: ListBlogsAll: many
+
+--name: GetBlog: one
+
+--name: GetBlogDeleted: one
+
+--name: UpdateBlog: one
+
+--name: DeleteBlogSoft: one
+
+--name: DeleteBlog: one
+
+--name: RestoreBlog: one
+
 -- hmmm... should I just return id or everything
 --  - content might be very large
 --  - will I actually use this retruned data ??
