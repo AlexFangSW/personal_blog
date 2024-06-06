aaf4d40dbda254cbe3f9889d07ec8eda6a5f8863
AlexFangSW <alexfangsw@gmail.com>
2024-06-06 14:48:45 +0800

    feat: implement models for blog_tags and blog_topics

--- backend/db/models/sqlite/blogTags.go
+++ backend/db/models/sqlite/blogTags.go
@@ -5,6 +5,8 @@ import (
 	"context"
 	"database/sql"
 	"fmt"
+	"log/slog"
+	"strconv"
 	"strings"
 )
 
@@ -50,9 +52,72 @@ func (b *BlogTags) Upsert(ctx context.Context, tx *sql.Tx, blogID int, tagIDs []
 }
 
 func (b *BlogTags) Delete(ctx context.Context, tx *sql.Tx, blogID int, tagIDs []int) error {
+	stmt := `DELETE FROM blog_tags WHERE blog_id = ? `
+
+	if len(tagIDs) == 0 {
+		// DELETE FROM blog_tags WHERE blog_id = ? ;
+		stmt += ";"
+
+	} else {
+		var inIDs strings.Builder
+
+		// DELETE FROM blog_tags WHERE blog_id = ? AND tag_id IN (x,x,x,x,x);
+		inIDs.WriteString("AND tag_id IN (")
+		for i, id := range tagIDs {
+			inIDs.WriteString(strconv.Itoa(id))
+			if i != len(tagIDs)-1 {
+				inIDs.WriteString(",")
+			}
+		}
+		inIDs.WriteString(");")
+
+		stmt += inIDs.String()
+	}
+	util.LogQuery(ctx, "DeleteBlogTags:", stmt)
+
+	res, err := tx.ExecContext(ctx, stmt, blogID)
+	if err != nil {
+		return fmt.Errorf("Delete: exec context failed: %w", err)
+	}
+	affectedRows, err := res.RowsAffected()
+	if err != nil {
+		return fmt.Errorf("Delete: aquire affected rows failed: %w", err)
+	}
+	slog.Debug("affected rows", "rows", affectedRows)
+
 	return nil
 }
 
 func (b *BlogTags) InverseDelete(ctx context.Context, tx *sql.Tx, blogID int, tagIDs []int) error {
+	if len(tagIDs) == 0 {
+		return nil
+	}
+
+	var values strings.Builder
+	for i, id := range tagIDs {
+		values.WriteString(strconv.Itoa(id))
+		if i != len(tagIDs) {
+			values.WriteString(",")
+		}
+	}
+
+	stmt := `
+	DELETE FROM blog_tags
+	WHERE 
+		blog_id = ?
+	AND tag_id NOT IN (` + values.String() + `)`
+
+	util.LogQuery(ctx, "InverseDeleteBlogTags:", stmt)
+
+	res, err := tx.ExecContext(ctx, stmt, blogID)
+	if err != nil {
+		return fmt.Errorf("InverseDelete: exec context failed: %w", err)
+	}
+	affectedRows, err := res.RowsAffected()
+	if err != nil {
+		return fmt.Errorf("InverseDelete: aquire affected rows failed: %w", err)
+	}
+	slog.Debug("affected rows", "rows", affectedRows)
+
 	return nil
 }
--- backend/db/models/sqlite/blogTopics.go
+++ backend/db/models/sqlite/blogTopics.go
@@ -5,6 +5,8 @@ import (
 	"context"
 	"database/sql"
 	"fmt"
+	"log/slog"
+	"strconv"
 	"strings"
 )
 
@@ -50,8 +52,71 @@ func (b *BlogTopics) Upsert(ctx context.Context, tx *sql.Tx, blogID int, topicID
 }
 
 func (b *BlogTopics) Delete(ctx context.Context, tx *sql.Tx, blogID int, topicIDs []int) error {
+	stmt := `DELETE FROM blog_topics WHERE blog_id = ? `
+
+	if len(topicIDs) == 0 {
+		// DELETE FROM blog_topics WHERE blog_id = ? ;
+		stmt += ";"
+
+	} else {
+		var inIDs strings.Builder
+
+		// DELETE FROM blog_topics WHERE blog_id = ? AND topic_id IN (x,x,x,x,x);
+		inIDs.WriteString("AND topic_id IN (")
+		for i, id := range topicIDs {
+			inIDs.WriteString(strconv.Itoa(id))
+			if i != len(topicIDs)-1 {
+				inIDs.WriteString(",")
+			}
+		}
+		inIDs.WriteString(");")
+
+		stmt += inIDs.String()
+	}
+	util.LogQuery(ctx, "DeleteBlogTopics:", stmt)
+
+	res, err := tx.ExecContext(ctx, stmt, blogID)
+	if err != nil {
+		return fmt.Errorf("Delete: exec context failed: %w", err)
+	}
+	affectedRows, err := res.RowsAffected()
+	if err != nil {
+		return fmt.Errorf("Delete: aquire affected rows failed: %w", err)
+	}
+	slog.Debug("affected rows", "rows", affectedRows)
+
 	return nil
 }
 func (b *BlogTopics) InverseDelete(ctx context.Context, tx *sql.Tx, blogID int, topicIDs []int) error {
+	if len(topicIDs) == 0 {
+		return nil
+	}
+
+	var values strings.Builder
+	for i, id := range topicIDs {
+		values.WriteString(strconv.Itoa(id))
+		if i != len(topicIDs) {
+			values.WriteString(",")
+		}
+	}
+
+	stmt := `
+	DELETE FROM blog_topics
+	WHERE 
+		blog_id = ?
+	AND topic_id NOT IN (` + values.String() + `)`
+
+	util.LogQuery(ctx, "InverseDeleteBlogTags:", stmt)
+
+	res, err := tx.ExecContext(ctx, stmt, blogID)
+	if err != nil {
+		return fmt.Errorf("InverseDelete: exec context failed: %w", err)
+	}
+	affectedRows, err := res.RowsAffected()
+	if err != nil {
+		return fmt.Errorf("InverseDelete: aquire affected rows failed: %w", err)
+	}
+	slog.Debug("affected rows", "rows", affectedRows)
+
 	return nil
 }
