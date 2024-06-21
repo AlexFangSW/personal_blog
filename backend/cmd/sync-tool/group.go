638c27fc399dccbd8394d8ca6e0326488ee8de53
AlexFangSW <alexfangsw@gmail.com>
2024-06-21 21:19:31 +0800

    wip(sync-tool): group topics

--- backend/cmd/sync-tool/group.go
+++ backend/cmd/sync-tool/group.go
@@ -1,3 +1,7 @@
+// In this project, the local copy of blogs, tags and topics are the source of truth.
+// The server, frontend and database are all just a way to organize the data and show our content.
+// The only thing we need to persist is a folder (such as the 'dummyData' folder),
+// which stores markdown and yaml files that can easily be persisted by pushing to GitHub
 package main
 
 import (
@@ -19,11 +23,13 @@ type Groups[T GroupTypes] struct {
 func groupTags(localTags []entities.InTag, tags []entities.Tag) (Groups[entities.Tag], error) {
 	slog.Debug("groupTags")
 
+	// for faster lookup
 	tagMap := map[string]entities.Tag{}
 	for _, tag := range tags {
 		tagMap[tag.Name] = tag
 	}
 
+	// go throgh local records, match them to remote data and categorize them
 	result := Groups[entities.Tag]{}
 	for _, localTag := range localTags {
 		remoteTag, ok := tagMap[localTag.Name]
@@ -48,6 +54,7 @@ func groupTags(localTags []entities.InTag, tags []entities.Tag) (Groups[entities
 		}
 	}
 
+	// the remaining remote data should be deleted
 	for _, remoteTag := range tagMap {
 		result.delete = append(result.delete, remoteTag)
 	}
@@ -56,6 +63,18 @@ func groupTags(localTags []entities.InTag, tags []entities.Tag) (Groups[entities
 }
 
 func groupTopics(localTopics []entities.InTopic, topics []entities.Topic) (Groups[entities.Topic], error) {
+	slog.Debug("groupTopics")
+
+	topicMap := map[string]entities.Topic{}
+	for _, topic := range topics {
+		topicMap[topic.Name] = topic
+	}
+
+	result := Groups[entities.Topic]{}
+	for _, localTopic := range localTopics {
+
+	}
+
 	return Groups[entities.Topic]{}, nil
 }
 
