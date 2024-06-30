495622d3c277d5605669400ec553a5dd2649c66b
AlexFangSW <alexfangsw@gmail.com>
2024-06-30 08:18:33 +0800

    feat(FE): default functionality finished

--- frontend/src/app/blogs/[id]/page.js
+++ frontend/src/app/blogs/[id]/page.js
@@ -1,73 +1,80 @@
 import { merianda } from "@/app/fonts";
 import Link from "next/link";
 import { MDXRemote } from "next-mdx-remote/rsc";
-import { promises as fs } from "fs";
+
+/**
+ * @param {Object} param0 
+ * @param {Object} param0.tags 
+ */
 function BlogTags({ tags }) {
-  // list of tags
   const tag_list = [];
   for (const tag of tags) {
-    tag_list.push(<div className="badge badge-outline">{tag}</div>);
+    tag_list.push(<div className="badge badge-outline">
+      {tag.name}
+    </div>);
   }
   return tag_list;
 }
 
+/**
+ * @param {Object} param0 
+ * @param {Object} param0.topics 
+ */
 function BlogTopics({ topics }) {
-  // list of topics
   const topic_list = [];
   for (const topic of topics) {
     topic_list.push(
-      <Link className="badge badge-outline" href={`/topics/${topic}`}>
-        {topic}
+      <Link className="badge badge-outline" href={`/topics/${topic.id}`}>
+        {topic.name}
       </Link>,
     );
   }
   return topic_list;
 }
 
+/**
+ * @param {int} id 
+ */
 async function getBlogData(id) {
-  // MDX text - can be from a local file, database, CMS, fetch, anywhere...
-  // const res = await fetch('https://...')
-  // const markdown = await res.text()
+  const url = `${process.env.BACKEND_BASE_URL}/blogs/${id}`
+  const res = await fetch(url)
+  const parsedRes = await res.json()
 
   // NOTE: markdown rander can't handle '{}' in content, unless we escape them with '\'
-  const file = await fs.readFile(
-    process.cwd() + "/src/app/test_blog.md",
-    "utf8",
-  );
-  const data = file;
-  return { data };
+  return parsedRes.msg;
 }
 
 export default async function Page({ params }) {
-  const { data } = await getBlogData(params.id);
+  const blog = await getBlogData(params.id);
 
   const customizeComponents = {
     p: (props) => <p className="text-xl">{props.children}</p>,
+    strong: (props) => <strong className="text-inherit">{props.children}</strong>,
   };
 
   return (
     <div className="flex flex-col min-h-screen items-center p-5">
       <div className="flex flex-col items-center gap-y-5">
         <h1 className={`title text-6xl ${merianda.className}`}>
-          Blog number: {params.id}
+          {blog.title}
         </h1>
         <div className="flex flex-row flex-wrap justify-center items-center gap-2">
           <p>By: AlexFangSW</p>
           <div className="divider divider-horizontal"></div>
           <div className="flex flex-col flex-wrap justify-center gap-2">
-            <p>Created at: 2024-06-14T11:39:56+00:00</p>
-            <p>Updated at: 2024-06-14T11:39:56+00:00</p>
+            <p>Created at: {blog.created_at}</p>
+            <p>Updated at: {blog.updated_at}</p>
           </div>
         </div>
-        <p>blog description aaaa bbbb cccc fjdskalfjdkslajfkdlsa</p>
+        <p>{blog.description}</p>
         <div className="flex flex-row flex-wrap justify-center gap-2">
-          <BlogTopics topics={["topic12"]} />
+          <BlogTopics topics={blog.topics} />
           <div className="divider divider-horizontal"></div>
-          <BlogTags tags={["aaa", "aaa", "aaa"]} />
+          <BlogTags tags={blog.tags} />
         </div>
         <div className="divider">Content</div>
         <article className="prose max-w-4xl  text-neutral-content">
-          <MDXRemote source={data} components={customizeComponents} />
+          <MDXRemote source={blog.content} components={customizeComponents} />
         </article>
       </div>
     </div>
