f64078f78960c662176e900edb56f848d81745e2
AlexFangSW <alexfangsw@gmail.com>
2024-09-23 10:43:30 +0800

    fix(BE): dockerfile

--- backend/scripts/docker_build.sh
+++ backend/scripts/docker_build.sh
@@ -4,4 +4,4 @@ set -a
 source ./.env
 set +a
 
-docker build -t "$DOCKER_REPO/$DOCKER_PROJECT/$DOCKER_IMAGE:$DOCKER_TAG" .
+docker build --network host -t "$DOCKER_REPO/$DOCKER_PROJECT/$DOCKER_IMAGE:$DOCKER_TAG" .
--- backend/scripts/docker_run.sh
+++ backend/scripts/docker_run.sh
@@ -5,6 +5,7 @@ source ./.env
 set +a
 
 docker run -d --rm --name blog-server \
+  --network test \
   --mount type=bind,source="./config.json",target="/app/config.json" \
   --mount type=bind,source="./blog.db",target="/app/blog.db" \
   -p 8080:8080 \
