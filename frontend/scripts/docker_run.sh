41662c49476d1ea448c12da41805ddfa773d25df
AlexFangSW <alexfangsw@gmail.com>
2024-09-26 17:45:28 +0800

    feat: enviroment variables for frontend

--- frontend/scripts/docker_run.sh
+++ frontend/scripts/docker_run.sh
@@ -5,6 +5,7 @@ source ./.env
 set +a
 
 docker run --rm --name blog-frontend \
+  --env-file ./.env \
   --network test \
   -p 3000:3000 \
   "$DOCKER_REPO/$DOCKER_PROJECT/$DOCKER_IMAGE:$DOCKER_TAG"
