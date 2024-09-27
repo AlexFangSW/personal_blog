62437b1e16e327db828e4902bfbe9b2bccdd4eb2
AlexFangSW <alexfangsw@gmail.com>
2024-09-27 21:14:07 +0800

    resize frontend blog width

--- frontend/src/app/blogs/[id]/[slug]/page.js
+++ frontend/src/app/blogs/[id]/[slug]/page.js
@@ -81,7 +81,7 @@ export default async function Page({ params }) {
         </div>
       </div>
       <div className="divider" >Content</div>
-      <div className="prose w-full lg:w-7/12 lg:max-w-none prose-strong:text-neutral-content text-neutral-content" style={{ overflowWrap: "break-word" }} dangerouslySetInnerHTML={{ __html: currentBlog.content }}></div>
+      <div className="prose w-full prose-strong:text-neutral-content text-neutral-content" style={{ overflowWrap: "break-word" }} dangerouslySetInnerHTML={{ __html: currentBlog.content }}></div>
     </div >
   )
 }
