2791cc764c5b0bd2d76a1f41b68ae0db01fb2dc3
AlexFangSW <alexfangsw@gmail.com>
2024-10-18 20:39:58 +0800

    fix: build error with google ananlytics
    Use component from next/third-parties for google ananlytics

--- frontend/src/app/layout.js
+++ frontend/src/app/layout.js
@@ -3,6 +3,7 @@ import { BlogFooter } from "./components/footer"
 import { BlogNav } from "./components/nav"
 import { ErrorBoundary } from "next/dist/client/components/error-boundary"
 import ErrorPage from "./error"
+import { GoogleAnalytics } from '@next/third-parties/google'
 
 export const metadata = {
   title: "Coding Notes",
@@ -16,14 +17,7 @@ export default function RootLayout({ children }) {
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta name="google-site-verification" content="_fWGovQfPY9FNJH0U2_DaOt2hkLyIpB3zJlC2b6Jcyw" />
       </head>
-      <script async src="https://www.googletagmanager.com/gtag/js?id=G-C15NTGQ9XB"></script>
-      <script>
-        window.dataLayer = window.dataLayer || [];
-        function gtag(){dataLayer.push(arguments)}
-        gtag('js', new Date());
-
-        gtag('config', 'G-C15NTGQ9XB');
-      </script>
+      <GoogleAnalytics gaId="G-C15NTGQ9XB" />
       <body>
         <BlogNav />
         <ErrorBoundary fallback={<ErrorPage />} >
